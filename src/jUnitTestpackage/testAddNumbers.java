c62ddd574d82b9ea85197bb5b01fe9ed6d245c33
mohitchaudhary <mohitchaudhary@localhost>
2022-11-18 11:51:08 +0530

    JunitTest

---
+++ src/jUnitTestpackage/testAddNumbers.java
@@ -0,0 +1,17 @@
+package jUnitTestpackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testAddNumbers {
+
+	@Test
+	public void test() {
+		jUnitFunctions junit = new jUnitFunctions();
+		int result = junit.addNumbers(100,200);
+		assertEquals(300, result);
+		
+	}
+
+}
